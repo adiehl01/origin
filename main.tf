a86ebf0c320bb0bd569d918087fc91e6479e2648
Aaron Diehl <aaron.diehl@oracle.com>
2025-01-14 12:58:47 -0500

    Added AWS Security Group and Ubuntu machine image with busybox webserver

--- main.tf
+++ main.tf
@@ -3,11 +3,34 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-#  ami           = "ami-0fb653ca2d3203ac1"
-  ami           = "ami-01816d07b1128cd2d"
-  instance_type = "t2.micro"
-  tags = {
-      Name = "terraform-example"
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_instance" "example" {
+# Amazon Image         ami               = "ami-0fb653ca2d3203ac1"
+# Book recommendation  ami           	 = "ami-01816d07b1128cd2d"
+  ami                    = "ami-005fc0f236362e99f"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  user_data_replace_on_change = true
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
